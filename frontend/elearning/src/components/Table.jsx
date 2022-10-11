ea490dc1e1000b2ee86c16404a117690ab834c3a
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-11 23:45:36 +0300

    Frontend => adding table component

---
+++ frontend/elearning/src/components/Table.jsx
@@ -0,0 +1,29 @@
+export default function Table({ rows, setRows }) {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>role</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows?.map((row, i) => (
+            <tr key={i}>
+              <td>{++i}</td>
+              <td>{row.name}</td>
+              <td>{row.email}</td>
+              <td>{row.password}</td>
+              <td>{row.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
