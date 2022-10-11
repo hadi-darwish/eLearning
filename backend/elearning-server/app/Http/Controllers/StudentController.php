fe07804e166c0ad2154f45e9a747a499ca943b9d
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-11 23:22:57 +0300

    Backend => adding get personal cources function to student controller

--- backend/elearning-server/app/Http/Controllers/StudentController.php
+++ backend/elearning-server/app/Http/Controllers/StudentController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class StudentController extends Controller
 {
-    //
+    public function getAllCourses()
+    {
+        $user = Auth::user();
+        $courses = $user->courses;
+        return response()->json([
+            'status' => 'success',
+            'courses' => $courses,
+        ]);
+    }
 }
