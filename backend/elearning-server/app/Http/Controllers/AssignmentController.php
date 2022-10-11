3a2c7d1305c497c26ac1963d83e7606535c0d24f
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-11 22:51:40 +0300

    Backend => adding create assignment function to controller

--- backend/elearning-server/app/Http/Controllers/AssignmentController.php
+++ backend/elearning-server/app/Http/Controllers/AssignmentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assignment;
 use Illuminate\Http\Request;
 
 class AssignmentController extends Controller
 {
-    //
+    public function createAssignment(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'course_id' => 'required|string',
+            'instructor_id' => 'required|string',
+        ]);
+        $assignment = Assignment::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'course_id' => $request->course_id,
+            'instructor_id' => $request->instructor_id,
+        ]);
+        return response()->json([
+            'status' => 'success',
+            'assignment' => $assignment,
+        ]);
+    }
 }
